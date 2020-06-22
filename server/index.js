168da421b9eebee9ac266b8f4df91ad5197bffb1
JanseyGarita <janseygarita@gmail.com>
2020-06-22 09:39:22 -0600

    Agrego morgan

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+const { request } = require('express');
 const app = express();
-
 
 //Settings
 app.set('port',process.env.PORT || 3000);
+
 //Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
 
 //Routes
 
