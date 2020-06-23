afd59a7f658e7bdfc3d0bf451c01e288df135a75
JanseyGarita <janseygarita@gmail.com>
2020-06-23 15:23:06 -0600

    Diseño de la tabla

--- client/src/app/components/person/person.component.ts
+++ client/src/app/components/person/person.component.ts
@@ -18,49 +18,41 @@ export class PersonComponent implements OnInit {
   }
 
   addPerson(formData: NgForm) {
-
-    if(formData.value._id){
-      this.personService.putPerson(formData.value).
-      subscribe(res=>{
+    if (formData.value._id) {
+      this.personService.putPerson(formData.value).subscribe((res) => {
         this.resetForm(formData);
-        M.toast({html:'Updated!'});
+        M.toast({ html: 'Updated!' });
         this.getPeople();
       });
-    }else{
-    this.personService.postPerson(formData.value)
-    .subscribe(res=>{
-      this.resetForm(formData);
-      M.toast({html:'Saved!'});
-      this.getPeople();
-    });
-  }
+    } else {
+      this.personService.postPerson(formData.value).subscribe((res) => {
+        this.resetForm(formData);
+        M.toast({ html: 'Saved!' });
+        this.getPeople();
+      });
+    }
   }
 
-  getPeople(){
-
-    this.personService.getPeople()
-    .subscribe(res=>{
+  getPeople() {
+    this.personService.getPeople().subscribe((res) => {
       this.personService.people = res as Person[];
     });
   }
 
-editPerson(person : Person){
-this.personService.selectedPerson = person;
-}
-
-
-deletePerson(_id:string){
-  if(confirm('Are you sure you want to delete it?')){
-this.personService.deletePerson(_id).
-subscribe(res=>{
-
-  M.toast({html:'Deleted!'});
-  this.getPeople();
-});
+  editPerson(person: Person) {
+    this.personService.selectedPerson = person;
   }
-}
-  resetForm(form?: NgForm){
-    if(form){
+
+  deletePerson(_id: string) {
+    if (confirm('Are you sure you want to delete it?')) {
+      this.personService.deletePerson(_id).subscribe((res) => {
+        M.toast({ html: 'Deleted!' });
+        this.getPeople();
+      });
+    }
+  }
+  resetForm(form?: NgForm) {
+    if (form) {
       form.reset();
       this.personService.selectedPerson = new Person();
     }
