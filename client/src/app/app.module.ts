b8716af8268310d085a58aa3e465e0ea02911979
Keilor2019 <57534883+Keilor2019@users.noreply.github.com>
2020-06-22 23:09:24 -0600

    Creacion de Models y Services

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { PersonComponent } from './components/person/person.component';
 
 import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent, PersonComponent],
   imports: [BrowserModule, FormsModule],
